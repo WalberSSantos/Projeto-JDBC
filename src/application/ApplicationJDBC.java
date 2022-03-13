1511f20265dbe9756160c649bab71940344025ae
Walber <walber_santos@hotmail.com>
2022-03-13 20:59:40 -0300

    Createing project and git repository

---
+++ src/application/ApplicationJDBC.java
@@ -0,0 +1,16 @@
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ApplicationJDBC extends Application {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+}
