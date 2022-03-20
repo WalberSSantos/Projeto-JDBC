3d18e5908aa490deab6df6dda7da0acbf6b2f94c
Walber <walber_santos@hotmail.com>
2022-03-20 19:18:57 -0300

    Reusing instatiation

--- src/application/ApplicationJDBC.java
+++ src/application/ApplicationJDBC.java
@@ -1,35 +1,28 @@
 package application;
 
-import java.util.Date;
 import java.util.Locale;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class ApplicationJDBC extends Application {
     public static void main(String[] args) throws Exception {
 
         Locale.setDefault(Locale.US);
-        
-        Department obj = new Department(1, "Books");
-
-        Seller seller = new Seller(21, "Walber", "walber@gmail.com", new Date(), 3000.0, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("=== TEST 01 === Seller findById ===");
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+ 
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Department obj = new Department(1, "Books");
-        Seller seller = new Seller(21, "Walber", "walber@gmail.com", new Date(), 3000.0, obj);
-        System.out.println(seller);
-        
-        
+    
     }
 }
+
